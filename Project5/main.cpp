1f7f18983479d384b28105e92aafc83290f2ba12
Jacob Courtney <jacobc@ou.edu>
2019-04-22 17:11:09 -0500

    Initial Commit

---
+++ Project5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project5
+//
+//  Created by Jacob Courtney on 4/22/19.
+//  Copyright © 2019 Jacob Courtney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
